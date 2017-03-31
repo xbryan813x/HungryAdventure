f6c5a0e8cf62ff1da22332131cd28974bf1f8ea6
ThinkFWD <mike.thinkfwd@gmail.com>
2017-03-31 16:16:57 -0400

    added and fixed form and code

--- client/src/components/searchForm.js
+++ client/src/components/searchForm.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {connect} from 'react-redux'
 import { Field, reduxForm } from 'redux-form'
 import DropdownList from '../../../node_modules/react-widgets/lib/DropdownList'
 import DateTimePicker from '../../../node_modules/react-widgets/lib/DateTimePicker'
@@ -13,9 +14,8 @@ const colors = [ { color: 'Red', value: 'ff0000' },
   { color: 'Green', value: '00ff00' },
   { color: 'Blue', value: '0000ff' } ]
 
-const renderDateTimePicker = ({ input: { onChange, value }, showTime }) =>
+const renderDateTimePicker = ({ input: { value }, showTime }) =>
   <DateTimePicker
-    onChange={onChange}
     format="DD MMM YYYY"
     time={showTime}
     value={!value ? null : new Date(value)}
@@ -46,7 +46,7 @@ let ReactWidgetsForm = props => {
         />
       </div>
       <div>
-        <button type="submit" disabled={pristine || submitting}>Submit</button>
+        <button type="submit" disabled={pristine || submitting}> Submit </button>
         <button type="button" disabled={pristine || submitting} onClick={reset}>Reset Values
         </button>
       </div>
@@ -58,42 +58,5 @@ ReactWidgetsForm = reduxForm({
   form: 'reactWidgets'  // a unique identifier for this form
 })(ReactWidgetsForm)
 
-export default ReactWidgetsForm
-
-
-
-
-
-
-// import React, { Component } from 'react';
-// import { Field, reduxForm } from 'redux-form';
-
-// class ContactForm extends Component {
-//   render() {
-//     const { handleSubmit } = this.props;
-//     return (
-//       <form onSubmit={handleSubmit}>
-//         <div>
-//           <label htmlFor="firstName">First Name</label>
-//           <Field name="firstName" component="input" type="text"/>
-//         </div>
-//         <div>
-//           <label htmlFor="lastName">Last Name</label>
-//           <Field name="lastName" component="input" type="text"/>
-//         </div>
-//         <div>
-//           <label htmlFor="email">Email</label>
-//           <Field name="email" component="input" type="email"/>
-//         </div>
-//         <button type="submit">Submit</button>
-//       </form>
-//     );
-//   }
-// }
-
-// // Decorate the form component
-// ContactForm = reduxForm({
-//   form: 'contact' // a unique name for this form
-// })(ContactForm);
-
-// export default ContactForm;
+// (state, action,)
+export default connect (null, null)(ReactWidgetsForm)
