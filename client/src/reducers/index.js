05cf0aad57f1d445761cefebe021c1da8cfc134a
Michael J Chan <mike.thinkfwd@gmail.com>
2017-03-31 15:05:04 -0400

    Revert "Added build folder where 'bundle.js' gets compiled to"

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,15 +1,16 @@
 import { combineReducers } from 'redux';
-// import { reducer as formReducer } from 'redux-form';
+import { reducer as formReducer } from 'redux-form';
 
 import destinations from './destinationsReducer';
 import user from './userReducer';
 
-// const reducers = {
-// 	form: formReducer
-// }
+const reducers = {
+	form: formReducer
+}
 
 export default combineReducers({
   destinations,
-  user
-  // reducers,
+  user,
+  reducers,
 });
+
