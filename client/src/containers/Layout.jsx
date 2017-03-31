336a6a29cb9733610ed278791c2124a06d16e4ab
ThinkFWD <mike.thinkfwd@gmail.com>
2017-03-31 17:03:07 -0400

    added form

--- client/src/containers/Layout.jsx
+++ client/src/containers/Layout.jsx
@@ -1,54 +1,37 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-
+//++++++ Imported Actions
 import { fetchUser } from '../actions/userAction';
 import { fetchDestinations } from '../actions/destinationsActions';
 
+//Imported Component
 import ContactFrom from '../components/searchForm';
 
-@connect((store) => {
-  return {
-  	user: store.user.user,
-  	userFetched: store.user.fetched,
-  	destinations: store.destinations.destinations,
-  }
-})
-//transpiling decorators (wrap components) --> inject props into layout without messing with layout component
-export default class Layout extends React.Component {
+class Layout extends React.Component {
   //Loads Immediately
   componentWillMount(){
-  	this.props.dispatch(fetchUser())
+    this.props.fetchUser()
   }
 
- fetchDestinations(){
-  	this.props.dispatch(fetchDestinations())
+ fetchDestinations = () => {
+    this.props.fetchDestinations()
   }
 
   submit = (values) => {
-    console.log(values)
+    console.log('hello');
   }
 
   render () {
-  	const { user, destinations } = this.props;
-    
-  	if(!destinations.length) {
-  		return <div>
-      <button onClick={ this.fetchDestinations.bind(this) }>load destinations</button>
-      <ContactFrom onSubmit={this.submit} />
-      <Link to='/work'>Link</Link>
-      <Link to='/test'>Link</Link>
-      </div>
-
-  	} else {
-      const mappedDestinations = destinations.map(destination => <li>{destination.text}</li>)
-
-
-      return <div>
-      
-      <h1> {user.name} </h1>
-        <ul>{mappedDestinations}</ul>
-      </div>
-    }
+    const { destinations } = this.props;
+     return <ContactFrom onSubmit={this.submit} />
   }
 }
+
+//Connects to store
+const mapStateToProps = ({destinations, user}) => ({
+ user: user.user,
+ userFetched: user.fetched,
+ destinations: destinations.destinations
+})
+
+export default connect(mapStateToProps, {fetchUser, fetchDestinations})(Layout);
