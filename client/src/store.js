c802db7fba875be9ceff776bbc4f4c17bd5a4e13
Vincent Liu <vince.liu38@gmail.com>
2017-04-05 18:13:16 -0400

    added redux dev tools

--- client/src/store.js
+++ client/src/store.js
@@ -1,5 +1,5 @@
-import { applyMiddleware, createStore } from 'redux';
-
+import { applyMiddleware, createStore, compose } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import ReduxLogger from 'redux-logger';
 import thunk from 'redux-thunk'; // allows you to fire off more than one action
 import promise from 'redux-promise-middleware';
@@ -8,4 +8,4 @@ import reducer from './reducers';
 
 const middleware = applyMiddleware(promise(), thunk, ReduxLogger)
 
-export default createStore(reducer, middleware)
+export default createStore(reducer, composeWithDevTools(middleware))
