07040dcd5cee3a6db3ff7f30e15abff1da52145e
ThinkFWD <mike.thinkfwd@gmail.com>
2017-04-05 12:36:30 -0400

    successfully rendered DestinationPage

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Layout from './containers/Layout';
 import Destinations from './containers/Destinations';
+import Destination from './containers/destinationPage';
 import GoogleMaps from './components/GoogleMaps';
 
 const Routes = () => (
@@ -10,6 +11,7 @@ const Routes = () => (
       <Route exact path="/" component={Layout} />
       <Route path="/flights" component={Destinations} />
       <Route path='/maps' component={GoogleMaps} />
+      <Route path='/destination' component={Destination} />
     </div>
   </Router>
 );
