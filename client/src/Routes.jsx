651b95f2614fb50ee5321ba324f45bdeb4e71576
ThinkFWD <mike.thinkfwd@gmail.com>
2017-04-05 12:46:37 -0400

    added maps

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Layout from './containers/Layout';
 import Destinations from './containers/Destinations';
 import Destination from './containers/destinationPage';
-import GoogleMaps from './components/GoogleMaps';
+import GoogleMaps from './containers/GoogleMaps';
 
 const Routes = () => (
   <Router>
