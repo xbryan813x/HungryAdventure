e32d946c0598373763bf059c6254f2c3c24775ed
Cameron <camdunne@gmail.com>
2017-04-05 12:08:00 -0400

    fix map require

---
+++ client/src/components/GoogleMaps.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+const  API = require('../keys/mapKey.js');
+
+const AnyReactComponent = ({ text }) => <div className="mapsText">{text}</div>;
+
+export default class GoogleMaps extends Component {
+
+  static defaultProps = {
+    center: {lat: 40.7127837, lng: -74.0059413},
+    zoom: 15
+  };
+  render() {
+
+    return (
+      <div className="maps">
+      <GoogleMapReact
+        defaultCenter={this.props.center}
+        defaultZoom={this.props.zoom}
+        bootstrapURLKeys={{key: API.googleMaps()}}
+      >
+        <AnyReactComponent
+          lat={40.714392}
+          lng={-74.006649}
+          text={'THIS IS YOUR PLACE'}
+        />
+
+      </GoogleMapReact>
+      </div>
+    );
+  }
+}
