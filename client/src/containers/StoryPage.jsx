396f0b1836d8474b8dd0f87c65e2ff565445ac43
Vincent Liu <vince.liu38@gmail.com>
2017-04-10 15:43:17 -0400

    rendering story page

--- client/src/containers/StoryPage.jsx
+++ client/src/containers/StoryPage.jsx
@@ -3,16 +3,26 @@ import { connect } from 'react-redux';
 
 class StoryPage extends Component {
 
+
   render() {
-    console.log(this.props);
+     console.log(this.props);
     return (
-      <div>hello</div>
+      <div>
+        <div>{this.props.destination.city}</div>
+        <div>{this.props.destination.country}</div>
+        <div>$ {this.props.destination.price}</div>
+        <div>{this.props.hotel.hotel}</div>
+        <div>$ {this.props.hotel.price}</div>
+        {this.props.events.map((event, i)=> {
+          return <div key={i}>{event.name}</div>
+        })}
+      </div>
     );
   }
 }
 
 const mapStateToProps = ({ current }) => ({
-  info: current,
+  ...current
 });
 
 export default connect(mapStateToProps, null)(StoryPage);
