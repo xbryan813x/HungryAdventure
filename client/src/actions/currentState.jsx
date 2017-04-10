b190f881fa3de199c356639b3d5becead86b6152
Vincent Liu <vince.liu38@gmail.com>
2017-04-10 16:05:52 -0400

    fixed bug where it didnt render story page

--- client/src/actions/currentState.jsx
+++ client/src/actions/currentState.jsx
@@ -1,11 +1,11 @@
-export function currentDestination(stateObj) {
-  return { type: 'FETCH_CURRENTDES_FULFILLED', payload: stateObj }
+export function currentDestination({ destination }) {
+  return { type: 'FETCH_CURRENTDES_FULFILLED', payload: destination };
 }
 
-export function currentHotel(stateObj) {
-  return { type: 'FETCH_CURRENTHOTEL_FULFILLED', payload: stateObj }
+export function currentHotel({ hotel }) {
+  return { type: 'FETCH_CURRENTHOTEL_FULFILLED', payload: hotel };
 }
 
-export function currentEvents(stateObj) {
-  return { type: 'FETCH_CURRENTEVENTS_FULFILLED', payload: stateObj }
+export function currentEvents({ event }) {
+  return { type: 'FETCH_CURRENTEVENTS_FULFILLED', payload: event };
 }
