51432923b7a8d1fd357807b4a903df752956c3f8
Cameron <camdunne@gmail.com>
2017-04-10 10:17:41 -0400

    refactor css files

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,28 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const minify = require('gulp-clean-css');
+const merge = require('merge-stream');
+
+gulp.task('sass', () => {
+  const scssStream = gulp.src('public/style/scss/style.scss')
+    .pipe(sass())
+    .pipe(concat('scss-files.scss'));
+
+  const cssStream = gulp.src('public/style/css/main.css')
+    .pipe(sass())
+    .pipe(concat('css-files.css'));
+
+  const mergedStream = merge(scssStream, cssStream)
+    .pipe(concat('style.css'))
+    .pipe(minify())
+    .pipe(gulp.dest('public/style/css'));
+
+  return mergedStream;
+});
 
 
-gulp.task('sass', () => gulp.src('public/style/scss/style.scss')
-  .pipe(sass())
-  .pipe(gulp.dest('public/style/css')));
-
-gulp.watch('public/style/scss/**/*.scss', ['sass']);
+gulp.watch([
+  'public/style/scss/style.scss',
+  'public/style/css/main.css',
+], ['sass']);
