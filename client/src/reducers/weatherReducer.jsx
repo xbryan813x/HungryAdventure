142fc61515c0ec53b561e43848db64af3fecdcc2
Bryam Pacheco <bpacheco@Bryams-MacBook-Pro.local>
2017-04-10 10:52:36 -0400

    WeatherComponents

---
+++ client/src/reducers/weatherReducer.jsx
@@ -0,0 +1,8 @@
+export default function reduce(state = {}, action) {
+  switch (action.type) {
+    case 'FETCH_WEATHER_FULFILLED' : {
+      return {...state, weather: action.payload }
+    }
+  }
+  return state;
+}
