dd83b129526b5610b3bddbac78cff4a4339bd8ef
ThinkFWD <mike.thinkfwd@gmail.com>
2017-04-11 17:37:30 -0400

    updated the front

--- client/src/containers/Layout.jsx
+++ client/src/containers/Layout.jsx
@@ -36,18 +36,205 @@ class Layout extends React.Component {
   render () {
      return (
       <div>
-        <div className='navlog'>
-          <Auth />
-        </div>
-        <center>
-          <div style={{ marginTop: '175px' }}>
-          <h1> HUNGRY ADVENTURE </h1>
-          <br></br>
-          <br></br>
-            <Search onSubmit={this.submit} />
-            
+      
+
+      <header>
+      <Auth />
+        <section id="search">
+          <div className="header-content">
+            <div className="header-content-inner">
+              <h1 id="homeHeading">HUNGRY ADVENTURE</h1>
+                <hr></hr>
+                  <center>
+                    <Search onSubmit={this.submit} />
+                  </center>
+            </div>
           </div>
-        </center>
+        </section>
+      </header>
+
+    <section className="bg-primary" id="about">
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 className="section-heading">Ready for an Adventure?</h2>
+                    <hr className="light"></hr>
+                    <p className="text-faded">Do you have a budget and data you're available but don't know where to go or what your options are? We have you covered! Out site provides a one stop shop for all travel
+                    needs.</p>
+                    <a href="#search" className="page-scroll btn btn-default btn-xl sr-button">Lets Get Started!</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="services">
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-12 text-center">
+                    <h2 className="section-heading">What We Do</h2>
+                    <hr className="primary"></hr>
+                </div>
+            </div>
+        </div>
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-3 col-md-6 text-center">
+                    <div className="service-box">
+                        <i className="fa fa-4x fa-diamond text-primary sr-icons"></i>
+                        <h3>Aggrigate</h3>
+                        <p className="text-muted">We aggrigate Skyscanner, Airbnb, Yelp, Google, Weather and more..</p>
+                    </div>
+                </div>
+                <div className="col-lg-3 col-md-6 text-center">
+                    <div className="service-box">
+                        <i className="fa fa-4x fa-paper-plane text-primary sr-icons"></i>
+                        <h3>Destinations</h3>
+                        <p className="text-muted">We generate options for you!</p>
+                    </div>
+                </div>
+                <div className="col-lg-3 col-md-6 text-center">
+                    <div className="service-box">
+                        <i className="fa fa-4x fa-newspaper-o text-primary sr-icons"></i>
+                        <h3>Stay and Play </h3>
+                        <p className="text-muted">We present affordable and fun options!</p>
+                    </div>
+                </div>
+                <div className="col-lg-3 col-md-6 text-center">
+                    <div className="service-box">
+                        <i className="fa fa-4x fa-heart text-primary sr-icons"></i>
+                        <h3>Story</h3>
+                        <p className="text-muted">You Construct a custom itinary and story</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section className="no-padding" id="locations">
+        <div className="container-fluid">
+            <div className="row no-gutter popup-gallery">
+                <div className="col-lg-4 col-sm-6">
+                    <a href="https://cdn.pixabay.com/photo/2016/10/18/08/13/travel-1749508_960_720.jpg" className="portfolio-box">
+                        <img src="https://cdn.pixabay.com/photo/2016/10/18/08/13/travel-1749508_960_720.jpg" className="img-responsive customImg" alt=""></img>
+                        <div className="portfolio-box-caption">
+                            <div className="portfolio-box-caption-content">
+                                <div className="project-category text-faded">
+                                    Category
+                                </div>
+                                <div className="project-name">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div className="col-lg-4 col-sm-6">
+                    <a href="https://cdn.pixabay.com/photo/2015/01/28/23/10/mosque-615415_960_720.jpg" className="portfolio-box">
+                        <img src="https://cdn.pixabay.com/photo/2015/01/28/23/10/mosque-615415_960_720.jpg" className="img-responsive customImg" alt=""></img>
+                        <div className="portfolio-box-caption">
+                            <div className="portfolio-box-caption-content">
+                                <div className="project-category text-faded">
+                                    Category
+                                </div>
+                                <div className="project-name">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div className="col-lg-4 col-sm-6">
+                    <a href="https://cdn.pixabay.com/photo/2017/01/20/00/30/maldives-1993704_960_720.jpg" className="portfolio-box">
+                        <img src="https://cdn.pixabay.com/photo/2017/01/20/00/30/maldives-1993704_960_720.jpg" className="img-responsive customImg" alt=""></img>
+                        <div className="portfolio-box-caption">
+                            <div className="portfolio-box-caption-content">
+                                <div className="project-category text-faded">
+                                    Category
+                                </div>
+                                <div className="project-name">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div className="col-lg-4 col-sm-6">
+                    <a href="https://cdn.pixabay.com/photo/2014/03/26/05/46/trevi-fountain-298411_960_720.jpg" className="portfolio-box">
+                        <img src="https://cdn.pixabay.com/photo/2014/03/26/05/46/trevi-fountain-298411_960_720.jpg" className="img-responsive customImg" alt=""></img>
+                        <div className="portfolio-box-caption">
+                            <div className="portfolio-box-caption-content">
+                                <div className="project-category text-faded">
+                                    Category
+                                </div>
+                                <div className="project-name">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div className="col-lg-4 col-sm-6">
+                    <a href="https://cdn.pixabay.com/photo/2017/03/14/04/31/rock-2141913_960_720.jpg" className="portfolio-box">
+                        <img src="https://cdn.pixabay.com/photo/2017/03/14/04/31/rock-2141913_960_720.jpg" className="img-responsive customImg" alt=""></img>
+                        <div className="portfolio-box-caption">
+                            <div className="portfolio-box-caption-content">
+                                <div className="project-category text-faded">
+                                    Category
+                                </div>
+                                <div className="project-name">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div className="col-lg-4 col-sm-6">
+                    <a href="https://cdn.pixabay.com/photo/2016/09/15/01/57/chiang-mai-1670926_960_720.jpg" className="portfolio-box">
+                        <img src="https://cdn.pixabay.com/photo/2016/09/15/01/57/chiang-mai-1670926_960_720.jpg" className="img-responsive customImg" alt=""></img>
+                        <div className="portfolio-box-caption">
+                            <div className="portfolio-box-caption-content">
+                                <div className="project-category text-faded">
+                                    Category
+                                </div>
+                                <div className="project-name">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+     <aside className="bg-dark">
+        <div className="container text-center">
+            <div className="call-to-action">
+                <h2>Development Team!</h2>
+            </div>
+        </div>
+    </aside>
+
+    <section id="contact">
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 className="section-heading">Development Team!</h2>
+                    <hr className="primary"></hr>
+                    <p>Get Ready as we introduce!</p>
+                </div>
+                <div className="col-lg-4 col-lg-offset-2 text-center">
+                    <i className="fa fa-phone fa-3x sr-contact"></i>
+                    <p>999-999-99</p>
+                </div>
+                <div className="col-lg-4 text-center">
+                    <i className="fa fa-envelope-o fa-3x sr-contact"></i>
+                    <p><a href="mailto:your-email@your-domain.com">team@hungryadventure.com</a></p>
+                </div>
+            </div>
+        </div>
+    </section>
+
       </div>
     );
   }
