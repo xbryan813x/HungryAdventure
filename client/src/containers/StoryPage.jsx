29f72429f6adf5cec3b4f80f281bc82dddb5ee60
Vincent Liu <vince.liu38@gmail.com>
2017-04-13 11:27:03 -0400

    changed to live data

--- client/src/containers/StoryPage.jsx
+++ client/src/containers/StoryPage.jsx
@@ -4,7 +4,6 @@ import { connect } from 'react-redux';
 class StoryPage extends Component {
 
   render() {
-    console.log(this.props);
     if (!this.props.hotel) {
       return (
         <div className="parallaxContainer">
--- server/config/routes.js
+++ server/config/routes.js
@@ -16,12 +16,12 @@ module.exports = (app, express) => {
   app.get('/api/viator', viator.scrape);
 
   // Activate for testing
-  app.get('/api/anywhere', anywhereController.getAnywhereDummy);
-  app.get('/api/hotels', hotelController.getHotelsDummy);
+  // app.get('/api/anywhere', anywhereController.getAnywhereDummy);
+  // app.get('/api/hotels', hotelController.getHotelsDummy);
 
   // //Live
-  // app.get('/api/anywhere', anywhereController.getAnywhere);
-  // app.get('/api/hotels', hotelController.getHotels);
+  app.get('/api/anywhere', anywhereController.getAnywhere);
+  app.get('/api/hotels', hotelController.getHotels);
 
 
   // Analysis
