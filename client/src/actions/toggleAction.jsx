edfed0600aa9acb2f97df982d242d1dedc838eaa
Cameron <camdunne@gmail.com>
2017-04-14 14:27:46 -0400

    feat(select): changes css class on selected hotel item

--- client/src/actions/toggleAction.jsx
+++ client/src/actions/toggleAction.jsx
@@ -2,3 +2,8 @@ export function toggleHotels({ hotels }) {
   const bool = (hotels === undefined || hotels === false);
   return { type: 'TOGGLE_HOTELS_FULLFILLED', payload: bool };
 }
+
+export function toggleSelect({ select, hotel }) {
+  const id = hotel.id;
+  return { type: 'TOGGLE_SELECT_FULLFILLED', payload: id };
+}
