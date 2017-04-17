3cb5c27845c513d1cf3becec58da10fc80844200
Cameron <camdunne@gmail.com>
2017-04-17 11:15:49 -0400

    style(story): consolidate conditional statements

--- client/src/containers/StoryPage.jsx
+++ client/src/containers/StoryPage.jsx
@@ -1,60 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import GoogleMapReact from 'google-map-react';
 
 class StoryPage extends Component {
-
   render() {
-    if (!this.props.hotel) {
-      return (
-        <div className="parallaxContainer">
-          <section className="parallax">
-            <h1 className="storyCity">{this.props.destination.city}</h1>
-            <div className="infoContainer">
-              {this.props.destination.country}
-              <div >{this.props.destination.city}</div>
-              <div>{this.props.destination.country}</div>
-              <div>$ {this.props.destination.price}</div>
-              {this.props.yelpEvents.map((event, i) => <div key={i}>{event.name}</div>)}
-            </div>
-            <div className="space" />
-          </section>
-        </div>
-      );
-    } else if (!this.props.events) {
-      return (
-        <div className="parallaxContainer">
-          <section className="parallax">
-            <h1 className="storyCity">{this.props.destination.city}</h1>
-            <div className="infoContainer">
-              {this.props.destination.country}
-              <div >{this.props.destination.city}</div>
-              <div>{this.props.destination.country}</div>
-              <div>$ {this.props.destination.price}</div>
-              <div>{this.props.hotel.hotel}</div>
-              <div>$ {this.props.hotel.price}</div>
-            </div>
-            <div className="space" />
-          </section>
-        </div>
-      );
-    } else if (!this.props.yelpEvent && !this.props.hotel) {
-      return (
-        <div className="parallaxContainer">
-          <section className="parallax">
-            <h1 className="storyCity">{this.props.destination.city}</h1>
-            <div className="infoContainer">
-              {this.props.destination.country}
-              <div >{this.props.destination.city}</div>
-              <div>{this.props.destination.country}</div>
-              <div>$ {this.props.destination.price}</div>
-            </div>
-            <div className="space" />
-          </section>
-        </div>
-      );
-    }
-
-
+    console.log('STOREYPAGEEEEFELWE', this.props.locator);
     return (
       <div className="parallaxContainer">
         <section className="parallax">
@@ -62,9 +12,18 @@ class StoryPage extends Component {
           <div className="infoContainer">
             {this.props.destination.country}
             <div>$ {this.props.destination.price}</div>
-            <div>{this.props.hotel.hotel}</div>
-            <div>$ {this.props.hotel.price}</div>
-            {this.props.yelpEvents.map((event, i) => <div key={i}>{event.name}</div>)}
+            {this.props.hotel ?
+              <div>
+                <div>$ {this.props.hotel.price}</div>
+                <div>{this.props.hotel.hotel}</div>
+              </div>
+              : '' }
+            {this.props.yelpEvents ?
+              <div>
+                {this.props.yelpEvents.map(event => <div key={event.name}>{event.name}</div>)}
+              </div>
+              : ''
+            }
           </div>
           <div className="space" />
         </section>
@@ -73,8 +32,9 @@ class StoryPage extends Component {
   }
 }
 
-const mapStateToProps = ({ current }) => ({
+const mapStateToProps = ({ current, geo }) => ({
   ...current,
+  ...geo,
 });
 
 export default connect(mapStateToProps, null)(StoryPage);
