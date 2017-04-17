1ede29f0e7a047b93ddcc0e5e5fd944b08850ac1
Bryam Pacheco <bpacheco@Bryams-MacBook-Pro.local>
2017-04-17 19:02:58 -0400

    rm event

--- client/src/components/Event.jsx
+++ client/src/components/Event.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Media, Col } from 'react-bootstrap';
 import StarRatingComponent from 'react-star-rating-component';
-import { YelpPin } from './Pins';
+import YelpPin from './YelpPin';
 import GoogleMaps from '../containers/GoogleMaps';
 import { currentEvents } from '../actions/currentStateAction';
 import { yelpBudget } from '../actions/budgetAction';
