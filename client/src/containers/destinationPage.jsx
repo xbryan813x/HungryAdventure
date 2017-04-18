51201255d273922cb9c7fb69abdc7fb0c1cfbcc6
Vincent Liu <vince.liu38@gmail.com>
2017-04-18 15:12:51 -0400

    cols working correctly

--- client/src/containers/destinationPage.jsx
+++ client/src/containers/destinationPage.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 // +++++ Imported Components
 import { Link } from 'react-router-dom';
-import { Col } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 import GoogleMaps from './GoogleMaps';
 import HotelList from '../components/HotelList';
 import Weather from '../components/weather';
@@ -24,56 +24,50 @@ class destinationPage extends Component {
     const foodCost = this.props.budget.yelpEvents || 0;
     const totalBudget = budget - flightCost - hotelCost - activityCost - foodCost;
     return (<div>
-      <h1> Hungry Adventure </h1>
+
       <div
         className="hero" style={{
-          backgroundImage: `url(${this.props.destination.imageUrl[0]})`,
+          background: `linear-gradient( rgba(0, 0, 0, 0.45), rgba(0, 0, 0, 0.45)), url(${this.props.destination.imageUrl[0]})`,
           height: '70%',
-
-        }}
-      ><DonutChart
-        data={[{ label: `Remaining ( $ ${totalBudget} )`,
-          value: totalBudget,
-          isEmpty: true,
-        },
-        { label: ` Hotel ( $ ${hotelCost} )`,
-          value: hotelCost },
-        { label: ` Flight ( $ ${flightCost} )`,
-          value: flightCost },
-        { label: `Attractions ( $ ${activityCost} )`,
-          value: activityCost },
-        { label: `Food ( $ ${foodCost} )`,
-          value: foodCost,
-        },
-        ]} height={200} width={200} legend={false} className="donutAlign"
-      />
-      </div>
-      <div
-        style={{
-          backgroundColor: 'white',
-          borderRadius: '15px',
-          margin: 'auto',
-          width: '50%',
-          textAlign: 'center',
-          top: '-5%',
+          'background-size': 'cover',
+          'margin-top': '-20px',
         }}
       >
-        {this.props.destination.IataCode}
+        <div className="titleContainer">
+          <h1>Hungry Adventure</h1>
+          <hr className="pageHr" />
+          <p className="pageTitle">{this.props.destination.city}, {this.props.destination.country}</p>
+        </div>
       </div>
-      <div className="container col-sm-offset-3" style={{ display: 'grid', marginBottom: '5%' }}>
-        <BudgetBar budget={this.props.budget} />
+
+      <div className="pageContainer">
+        <Col sm={4}><FrommersInfo /></Col>
+        <Col sm={4} className="donut"> <DonutChart
+          data={[{ label: `Remaining ( $ ${totalBudget} )`,
+            value: totalBudget,
+            isEmpty: true,
+          },
+          { label: ` Hotel ( $ ${hotelCost} )`,
+            value: hotelCost },
+          { label: ` Flight ( $ ${flightCost} )`,
+            value: flightCost },
+          { label: `Attractions ( $ ${activityCost} )`,
+            value: activityCost },
+          { label: `Food ( $ ${foodCost} )`,
+            value: foodCost,
+          },
+          ]} height={200} width={200} legend={false} className="donutAlign"
+        /></Col>
+        <Col sm={4}> <Weather /></Col>
       </div>
-      <div style={{ marginBottom: '10%' }}>
-        <Col sm={6} className="col-md-offset-1">
-          <FrommersInfo />
-        </Col>
-        <Weather />
-      </div>
-      <div style={{ display: 'grid' }} />
+
       <GoogleMaps locator={this.props.geo.locator} hotelsArr={this.props.hotels.hotels} />
       <HotelList hotels={this.props.hotels} destination={this.props.destination} />
       <ViatorEvents />
+      <br />
       <YelpEvents />
+
+
       <button> <Link to="/storypage"> CHECKOUT</Link></button>
     </div>
     );
