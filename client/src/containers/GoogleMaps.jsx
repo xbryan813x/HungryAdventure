646dd046feeccd5206b978028bc70805ccaa0f79
Cameron <camdunne@gmail.com>
2017-04-19 19:34:18 -0400

    feat(markers): icon markers

--- client/src/containers/GoogleMaps.jsx
+++ client/src/containers/GoogleMaps.jsx
@@ -1,25 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import GoogleMapReact from 'google-map-react';
-import shallowCompare from 'react-addons-shallow-compare';
 
-import { MyGreatPlaceWithStick } from '../../utils/markerPins';
-
-import { HotelPin } from '../components/Pins';
+import { HotelPin, YelpPin } from '../components/Pins';
 import { googleMaps } from '../keys/mapsKey';
 
 class GoogleMaps extends Component {
-  shouldComponentUpdate(nextProps, nextState) {
-    return shallowCompare(this, nextProps, nextState);
-  }
 
-  _distanceToMouse = (markerPos, mousePos, markerProps) => {
-    console.log(markerPos)
-    const x = markerPos.x;
-    const y = markerPos.y - 15 - 30 / 2;
-    const distanceKoef = markerProps.text !== 'A' ? 1.5 : 1;
-    return distanceKoef * Math.sqrt((x - mousePos.x) * (x - mousePos.x) + (y - mousePos.y) * (y - mousePos.y));
-  }
 
   render() {
     if (this.props.locator === undefined || this.props.hotels === undefined) {
@@ -28,20 +15,27 @@ class GoogleMaps extends Component {
       );
     }
     return (
-        <GoogleMapReact
-          options={{ scrollwheel: false }}
-          defaultCenter={{ lat: this.props.geo.locator.latitude, lng: this.props.geo.locator.longitude }}
-          defaultZoom={12}
-          bootstrapURLKeys={{ key: googleMaps() }}
-          hoverDistance={15}
-          distanceToMouse={this._distanceToMouse}
-        >
-          {this.props.hotelsArr.map((hotel, index) =>
-            <HotelPin
-              lat={hotel.lat} lng={hotel.lng} key={index} price={hotel.price}
-            />,
-        )}
-        </GoogleMapReact>
+      <GoogleMapReact
+        options={{ scrollwheel: false }}
+        defaultCenter={{ lat: this.props.geo.locator.latitude, lng: this.props.geo.locator.longitude }}
+        defaultZoom={11}
+        bootstrapURLKeys={{ key: googleMaps() }}
+      >
+        {this.props.mapArray.map((elem, index) => {
+          if (elem.lat) {
+            return (<HotelPin
+              lat={elem.lat}
+              lng={elem.lng}
+              key={elem.id}
+            />);
+          }
+          return (<YelpPin
+            lat={elem.coordinates.latitude}
+            lng={elem.coordinates.longitude}
+            key={elem.name}
+          />);
+        })}
+      </GoogleMapReact>
     );
   }
 }
--- client/src/containers/destinationPage.jsx
+++ client/src/containers/destinationPage.jsx
@@ -10,6 +10,7 @@ import BudgetBar from '../components/budgetBar';
 import FrommersInfo from './FrommersInfo';
 import ViatorEvents from './ViatorEvents';
 import YelpEvents from './YelpEvents';
+import { pinArray } from '../../utils/storyPageHelpers';
 
 // Charts
 import DonutChart from 'react-donut-chart';
@@ -27,13 +28,16 @@ class destinationPage extends Component {
     const activityCost = this.props.budget.viatorEvents || 0;
     const foodCost = this.props.budget.yelpEvents || 0;
     const totalBudget = budget - flightCost - hotelCost - activityCost - foodCost;
+
+    const mapArray = pinArray(this.props.current);
+    console.log('maparray', mapArray);
     return (<div>
       <div
         className="hero" style={{
           background: `linear-gradient( rgba(0, 0, 0, 0.45), rgba(0, 0, 0, 0.45)), url(${this.props.destination.imageUrl[0]}) no-repeat center center fixed`,
           height: '60%',
-          'background-size': 'cover',
-          'margin-top': '-20px',
+          backgroundSize: 'cover',
+          marginTop: '-20px',
         }}
       >
         <div className="titleContainer">
@@ -65,7 +69,7 @@ class destinationPage extends Component {
         <Col sm={4} xs={12} className="mobileSpacing"><FrommersInfo /></Col>
       </div>
 
-      <Col sm={12} xs={12} className="mapsPadding"><div className="maps"><GoogleMaps locator={this.props.geo.locator} hotelsArr={this.props.hotels.hotels} /></div></Col>
+      <Col sm={12} xs={12} className="mapsPadding"><div className="maps"><GoogleMaps locator={this.props.geo.locator} mapArray={mapArray} /></div></Col>
       <HotelList hotels={this.props.hotels} destination={this.props.destination} />
       <ViatorEvents />
       <YelpEvents />
