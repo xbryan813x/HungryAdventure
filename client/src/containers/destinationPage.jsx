8ebde5004c14e2f50273dde4995ee6c97c0db6b5
Vincent Liu <vince.liu38@gmail.com>
2017-04-19 19:06:36 -0400

    goes to story page from cart

--- client/src/containers/destinationPage.jsx
+++ client/src/containers/destinationPage.jsx
@@ -30,7 +30,7 @@ class destinationPage extends Component {
     const totalBudget = budget - flightCost - hotelCost - activityCost - foodCost;
     return (<div>
       {/* <Button style={{ borderRadius: '0', position: 'fixed', float: 'left', }} className="checkoutbutton"> <Link to="/storypage">Checkout</Link></Button> */}
-      <div className="circle"><div className="checkoutbutton glyphicon glyphicon-shopping-cart"><Link to="/storypage" /></div></div>
+      <Link to="/storypage"><div className="circle"><div className="checkoutbutton glyphicon glyphicon-shopping-cart"></div></div></Link>
       <div
         className="hero" style={{
           background: `linear-gradient( rgba(0, 0, 0, 0.45), rgba(0, 0, 0, 0.45)), url(${this.props.destination.imageUrl[0]}) no-repeat center center fixed`,
